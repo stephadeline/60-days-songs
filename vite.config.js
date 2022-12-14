4085400c08901c04a7a196c46ef47e22a479f41e
Matt Osborn <mattjosborn@gmail.com>
2022-12-14 10:29:25 -0600

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	optimizeDeps: {
-		esbuildOptions: {
-			target: 'es2020'
-		}
-	},
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
 
-	build: {
-		outDir: 'build'
-	},
-	plugins: [svelte()]
+  build: {
+    outDir: "build",
+    target: "es2020",
+  },
+  plugins: [svelte()],
 });
