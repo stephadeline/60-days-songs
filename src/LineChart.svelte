359d71837dc7b17fec9a58b7773471817490d636
Stephanie Adeline <stephadeline98@gmail.com>
2022-12-22 16:46:18 +0700

    update version

--- src/LineChart.svelte
+++ src/LineChart.svelte
@@ -4,4 +4,4 @@
 
 <h2>Line chart by Steph</h2>
 <iframe width="100%" height="966" frameborder="0"
-  src="https://observablehq.com/embed/f9b6545ddbf85d55@415?cells=viewof+selectedCountry%2Cwarn%2Cviewof+zoom%2Cviewof+isLog%2Cviewof+yVarLine%2CconsumptionChart%2Cviewof+wealthIndicator%2CindicatorChart"></iframe>
+  src="https://observablehq.com/embed/f9b6545ddbf85d55@421?cells=viewof+selectedCountry%2Cwarn%2Cviewof+zoom%2Cviewof+isLog%2Cviewof+yVarLine%2CconsumptionChart%2Cviewof+wealthIndicator%2CindicatorChart"></iframe>
