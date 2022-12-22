85ed93d4444feb0159357d8bf27bcfef298f8376
Stephanie Adeline <stephadeline98@gmail.com>
2022-12-22 17:28:00 +0700

    Update LineChart.svelte

--- src/LineChart.svelte
+++ src/LineChart.svelte
@@ -4,4 +4,4 @@
 
 <h2>Line chart by Steph</h2>
 <iframe width="100%" height="1008" frameborder="0"
-  src="https://observablehq.com/embed/f9b6545ddbf85d55@449?cells=viewof+selectedContinent%2Cviewof+selectedCountry%2Cviewof+zoom%2Cviewof+isLog%2Cwarn%2Cviewof+yVarLine%2CconsumptionChart%2Cviewof+wealthIndicator%2CindicatorChart"></iframe>
+  src="https://observablehq.com/embed/f9b6545ddbf85d55@451?cells=viewof+selectedContinent%2Cviewof+selectedCountry%2Cviewof+zoom%2Cviewof+isLog%2Cwarn%2Cviewof+yVarLine%2CconsumptionChart%2Cviewof+wealthIndicator%2CindicatorChart"></iframe>
