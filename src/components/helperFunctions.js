20dfcd1928d420e605dcc7ecd3d42b6f751d5147
Stephanie Adeline <stephadeline98@gmail.com>
2023-01-07 23:25:38 +0700

    fix the x axis a little bit

--- src/components/helperFunctions.js
+++ src/components/helperFunctions.js
@@ -24,6 +24,22 @@ export const formatNum = function (n) {
   });
 };
 
+export const formatAxisTicks = function (num) {
+  if (num >= 1000000000000) {
+    return (num / 1000000000000).toFixed(1).replace(/\.0$/, "") + "T";
+  }
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+  }
+  return num;
+};
+
 export const getFormattedValue = function (indicator, value) {
   let unit = units[indicator];
   if (unit === null) {
