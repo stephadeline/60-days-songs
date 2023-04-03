f7edf863da8ab076185b093715822d93ba2cef1d
sadeline <42850722+stephadeline@users.noreply.github.com>
2023-04-03 16:54:08 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -265,7 +265,7 @@
 
       <section>
         <p class="scrolly-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          TEST Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.{index + 1}
